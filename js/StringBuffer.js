f700f7eb32441ef4898cdc78a341bc206403f39f
Vigneswaran <vignes.marimuthu@gmail.com>
2013-03-20 22:28:23 +0530

    Bug fix and added StringBuffer utility methods.
    * Bug Fix.
    * Added 'replace', 'setCharAt', 'substring' methods.

--- js/StringBuffer.js
+++ js/StringBuffer.js
@@ -1,3 +1,10 @@
+/**
+ * StringBuffer Class.
+ * 
+ * @author Vigneswaran Marimuthu
+ * 
+ * @returns StringBuffer
+ */
 
 function StringBuffer(){
 	
@@ -36,7 +43,7 @@ function StringBuffer(){
 	
 	/**
 	 * Returns the character present at the passed index. Returns <code>null</code>,
-	 * if the passed index is not a number.
+	 * if the passed index is not a number or exceeds buffer length.
 	 * 
 	 * @author Vigneswaran Marimuthu
 	 * 
@@ -45,7 +52,7 @@ function StringBuffer(){
 	 * @returns String or <code>null</code>
 	 */
 	this['charAt'] = function(index){
-		if(isNumber(index)){
+		if(isNumber(index) && index < this.length()){
 			return buffer.join('').charAt(index);
 		}
 		return null;
@@ -54,7 +61,8 @@ function StringBuffer(){
 	/**
 	 * Deletes the characters present between the passed index. By default, 'end'
 	 * will be the length of the string buffer. Returns the deleted characters or 
-	 * <code>null</code>, if 'start' is not a number.
+	 * <code>null</code>, if 'start' is not a number or greater than the buffer
+	 * length.
 	 * 
 	 * @author Vigneswaran Marimuthu
 	 * 
@@ -64,18 +72,19 @@ function StringBuffer(){
 	 * @returns String or <code>null</code>
 	 */
 	this['delete'] = function(start, end){
-		if(isNumber(start)){
+		var len = this.length();
+		if(isNumber(start) && start < len){
 			var temp, str = buffer.join('');
 			buffer.clear();
-			temp = str.substring(0, start);		// TODO: Make sure start and end does not exceed the length of the string buffer.
-			if(isNumber(end)){
+			temp = str.substring(0, start);
+			if(isNumber(end) && end < len){
 				if(start < end){
-					str = temp.concat(str.substring(end+1));
-					buffer.push(str);
+					buffer.push(temp.concat(str.substring(end+1)));
 					return str.substring(start, end+1);
 				}
-				else console.debug('Start index should be less than end index.');	
+				else console.debug('Start index should be less than end index. Considering end index as the buffer length.');	
 			}
+			else if(end != null) console.debug('End index exceeds the buffer length. Discarding end index.');
 			buffer.push(temp);
 			return str.substring(start);
 		}
@@ -84,7 +93,8 @@ function StringBuffer(){
 	
 	/**
 	 * Deletes the character present at the passed index. Returns the deleted
-	 * character or <code>null</code>, if the passed index is not a number.
+	 * character or <code>null</code>, if the passed index is not a number or
+	 * exceeds buffer length.
 	 * 
 	 * @author Vigneswaran Marimuthu
 	 * 
@@ -93,7 +103,7 @@ function StringBuffer(){
 	 * @returns String or <code>null</code>
 	 */
 	this['deleteCharAt'] = function(index){
-		if(isNumber(index)){
+		if(isNumber(index) && index < this.length()){
 			var temp, str = buffer.join('');
 			buffer.clear();
 			temp = str.substring(0, index);
@@ -101,6 +111,7 @@ function StringBuffer(){
 			return str.substring(index, index+1);
 		}
 		return null;
+		/*return this['delete'](index, index);*/	// using internal delete method instead of separate method.
 	};
 	
 	/**
@@ -117,7 +128,7 @@ function StringBuffer(){
 	 */
 	this['indexOf'] = function(value, start){
 		if(isString(value)){
-			return buffer.join('').indexOf(value, isNumber(start) ? start : 0);
+			return buffer.join('').indexOf(value, (isNumber(start) && start < this.length()) ? start : 0);
 		}
 		return null;
 	};
@@ -134,7 +145,12 @@ function StringBuffer(){
 	 * @returns StringBuffer or <code>null</code>
 	 */
 	this['insert'] = function(index, value){
+		var len = this.length();
 		if(isNumber(index) && (isNumber(value) || isString(value))){
+			if(index >= len && index != 0){
+				console.log('Index exceeds the buffer length. Considering index as the buffer length.');
+				index = len - 1;
+			}
 			var temp, str = buffer.join('');
 			buffer.clear();
 			temp = str.substring(0, index);
@@ -159,7 +175,92 @@ function StringBuffer(){
 	this['lastIndexOf'] = function(value, start){
 		if(isString(value)){
 			var str = buffer.join('');
-			return str.lastIndexOf(value, isNumber(start) ? start : str.length);
+			return str.lastIndexOf(value, (isNumber(start) && start < this.length()) ? start : str.length);
+		}
+		return null;
+	};
+	
+	/**
+	 * Replace the contents between the start and the end index with the specified
+	 * value. By default, 'end' index is the length of the buffer. Returns StringBuffer
+	 * or <code>null</code>, if incorrect values are passed.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param String
+	 * @param Number
+	 * @param Number
+	 * 
+	 * @returns StringBuffer or <code>null</code>
+	 */
+	this['replace'] = function(value, start, end){
+		var len = this.length();
+		if(isString(value) && isNumber(start) && start < len){
+			var temp, str = buffer.join('');
+			buffer.clear();
+			temp = str.substring(0, start);
+			if(isNumber(end) && end < len){
+				if(start < end){
+					buffer.push(temp.concat(value).concat(str.substring(end+1)));
+					return this;
+					/*return str.substring(start, end+1);*/	// will return the replaced characters.
+				}
+				else console.debug('Start index should be less than end index. Considering end index as the buffer length.');	
+			}
+			else if(end != null) console.debug('End index exceeds the buffer length. Discarding end index.');
+			buffer.push(temp.concat(value));
+			return this;
+			/*return str.substring(start);*/	// will return the replaced characters.
+		}
+		return null;
+	};
+	
+	/**
+	 * Replaces the character present at index with the value. Returns StringBuffer
+	 * character or <code>null</code>, if incorrect parameters are passed.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param Number
+	 * @param String
+	 * 
+	 * @returns StringBuffer or <code>null</code>
+	 */
+	this['setCharAt'] = function(index, value){
+		if(isNumber(index) && index < this.length() && isString(value)){
+			var temp, str = buffer.join('');
+			buffer.clear();
+			temp = str.substring(0, index);
+			buffer.push(temp.concat(value).concat(str.substring(index+1)));
+			return this;
+			/*return this.replace(value, index, index);*/	// using internal replace method instead of separate method.
+		}
+		return null;
+	};
+	
+	/**
+	 * Returns the string present between the passed index. By default, the 'end'
+	 * index will be the buffer length. Returns StringBuffer
+	 * character or <code>null</code>, if incorrect parameters are passed.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param Number
+	 * @param Number
+	 * 
+	 * @returns String or <code>null</code>
+	 */
+	this['substring'] = function(start, end){
+		var len = this.length();
+		if(isNumber(start) && start < len){
+			var temp, str = buffer.join('');
+			temp = str.substring(start);
+			if(isNumber(end) && end < len){
+				if(start < end) temp = temp.substring(0, end);
+				else console.debug('Start index should be less than end index. Considering end index as the buffer length.');	
+			}
+			else if(end != null) console.debug('End index exceeds the buffer length. Discarding end index.');
+			return temp;
 		}
 		return null;
 	};
@@ -185,8 +286,6 @@ function StringBuffer(){
 	this.toString = function(){
 		return buffer.join('');
 	};
-	
-	// TODO: IMPLEMENT THE REST AND TEST
 	
 	/**
 	 * Private Utility Methods
